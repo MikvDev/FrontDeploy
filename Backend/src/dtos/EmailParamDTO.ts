64418da5f564f1ed5cd695644f22f44d9b216daa
Miguel Vargas Ferreira <miguelvargasferreira39@gmail.com>
2026-07-28 17:02:37 -0300

    Delete Backend directory

--- Backend/src/dtos/EmailParamDTO.ts
+++
@@ -1,5 +0,0 @@
-import z from 'zod'
-
-export const EmailParamDTO = z.object({
-    email: z.email('E-mail inválido')
-})
--- Backend/src/dtos/IdParamDTO.ts
+++
@@ -1,7 +0,0 @@
-import z from 'zod'
-
-export const IdParamDTO = z.object({
-    id: z.coerce.number()
-        .int('Id precisa ser um número inteiro')
-        .positive('Id precisa ser um número positivo')
-})
